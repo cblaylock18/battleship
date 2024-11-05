45e0793790c7dfea381b8268f437575f2f23be5e
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-11-05 16:23:16 -0500

    feat: add gameboard class and player class

--- src/classes.js
+++ src/classes.js
@@ -1,3 +1,10 @@
+// standard battleship has 5 ships
+//  carrier (5 holes)
+//  battleship (4 holes)
+//  cruiser (3 holes)
+//  submarine (3 holes)
+//  destroyer (2 holes)
+
 class Ship {
   constructor(length) {
     this.length = length;
@@ -14,6 +21,161 @@ class Ship {
   }
 }
 
-class Gameboard {}
+// Gameboard layout visual (called out in LETTER, NUMBER format)
+//   1 2 3 4 5 6 7 8 9 10
+// A
+// B
+// C
+// D
+// E
+// F
+// G
+// H
+// I
+// J
 
-export { Ship, Gameboard };
+class Gameboard {
+  constructor() {
+    this.carrier = {
+      ship: new Ship(5),
+      coordinates: [],
+      placed: false,
+    };
+    this.battleship = {
+      ship: new Ship(4),
+      coordinates: [],
+      placed: false,
+    };
+    this.cruiser = {
+      ship: new Ship(3),
+      coordinates: [],
+      placed: false,
+    };
+    this.submarine = {
+      ship: new Ship(3),
+      coordinates: [],
+      placed: false,
+    };
+    this.destroyer = {
+      ship: new Ship(2),
+      coordinates: [],
+      placed: false,
+    };
+    this.hits = [];
+    this.misses = [];
+  }
+
+  placeShip(letterCoordinate, numberCoordinate, orientation, shipType) {
+    const allowedLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    const letterIndex = allowedLetters.indexOf(letterCoordinate);
+
+    const shipLength = this[shipType].ship.length;
+    let shipCoordinates = [];
+    const existingCoordinates = this.listShipLocations();
+
+    if (
+      (orientation === "horizontal" && shipLength + numberCoordinate > 10) ||
+      (orientation === "vertical" && shipLength + letterIndex > 9) ||
+      letterIndex === -1 ||
+      numberCoordinate < 1 ||
+      numberCoordinate > 10
+    ) {
+      throw new Error("ships must be placed entirely on the board");
+    }
+
+    for (let i = 0; i < shipLength; i++) {
+      if (orientation === "horizontal") {
+        shipCoordinates.push([letterCoordinate, numberCoordinate + i]);
+      } else if (orientation === "vertical") {
+        shipCoordinates.push([
+          allowedLetters[letterIndex + i],
+          numberCoordinate,
+        ]);
+      }
+    }
+
+    shipCoordinates.forEach((newCoordinate) => {
+      existingCoordinates.forEach((oldCoordinate) => {
+        if (
+          newCoordinate[0] === oldCoordinate[0] &&
+          newCoordinate[1] === oldCoordinate[1]
+        ) {
+          throw new Error("ships cannot overlap");
+        }
+      });
+    });
+
+    this[shipType].coordinates = shipCoordinates;
+    this[shipType].placed = true;
+    // update DOM for ship location
+  }
+
+  receiveAttack(letterCoordinate, numberCoordinate) {
+    const shipTypes = this.listShipTypes();
+
+    shipTypes.forEach((ship) => {
+      this[ship].coordinates.forEach((coordinate) => {
+        if (
+          coordinate[0] === letterCoordinate &&
+          coordinate[1] === numberCoordinate
+        ) {
+          this.hits.push([letterCoordinate, numberCoordinate]);
+          this[ship].ship.hit();
+          //   update DOM for hit
+          return;
+        }
+      });
+    });
+
+    this.misses.push([letterCoordinate, numberCoordinate]);
+    // update DOM for miss
+  }
+
+  listShipLocations() {
+    return this.carrier.coordinates
+      .concat(this.battleship.coordinates)
+      .concat(this.cruiser.coordinates)
+      .concat(this.submarine.coordinates)
+      .concat(this.destroyer.coordinates);
+  }
+
+  listShipTypes() {
+    return ["carrier", "battleship", "cruiser", "submarine", "destroyer"];
+  }
+
+  placedShips() {
+    const allShipTypes = this.listShipTypes();
+    let placedShips = 0;
+
+    allShipTypes.forEach((shipType) => {
+      if (this[shipType].placed) {
+        placedShips++;
+      }
+    });
+
+    return placedShips;
+  }
+
+  allShipsSunk() {
+    let numberOfSunkenShips = 0;
+    let numberOfPlacedShips = this.placedShips();
+    const allShipTypes = this.listShipTypes();
+
+    allShipTypes.forEach((shipType) => {
+      if (this[shipType].ship.isSunk()) {
+        numberOfSunkenShips++;
+      }
+    });
+
+    return numberOfSunkenShips === numberOfPlacedShips;
+  }
+}
+
+class Player {
+  constructor(type) {
+    this.type = type;
+    this.gameboard = new Gameboard();
+  }
+}
+
+export { Ship, Gameboard, Player };
--- src/classes.test.js
+++ src/classes.test.js
@@ -1,4 +1,4 @@
-import { Ship, Gameboard } from "./classes.js";
+import { Ship, Gameboard, Player } from "./classes.js";
 
 describe("ship class tests", () => {
   let carrier;
@@ -49,13 +49,135 @@ describe("ship class tests", () => {
 });
 
 describe("gameboard class tests", () => {
-  let playerOneGameboard;
+  let gameboard;
 
-  beforeEach(() => {
-    playerOneGameboard = new Gameboard();
+  describe("placeShip tests", () => {
+    beforeEach(() => {
+      gameboard = new Gameboard();
+    });
+    test("place a carrier at A1 horizontally", () => {
+      gameboard.placeShip("A", 1, "horizontal", "carrier");
+      expect(gameboard.carrier.coordinates).toEqual([
+        ["A", 1],
+        ["A", 2],
+        ["A", 3],
+        ["A", 4],
+        ["A", 5],
+      ]);
+    });
+
+    test("place a carrier at C3 vertically", () => {
+      gameboard.placeShip("C", 3, "vertical", "carrier");
+      expect(gameboard.carrier.coordinates).toEqual([
+        ["C", 3],
+        ["D", 3],
+        ["E", 3],
+        ["F", 3],
+        ["G", 3],
+      ]);
+    });
+
+    test("place a destroyer at J10 vertically (should fail)", () => {
+      expect(() => {
+        gameboard.placeShip("J", 10, "vertical", "destroyer");
+      }).toThrow("ships must be placed entirely on the board");
+    });
+
+    test("place a destroyer at K(!)10 vertically (should fail)", () => {
+      expect(() => {
+        gameboard.placeShip("K", 10, "vertical", "destroyer");
+      }).toThrow("ships must be placed entirely on the board");
+    });
+
+    test("place a destroyer at A-10(!) vertically (should fail)", () => {
+      expect(() => {
+        gameboard.placeShip("A", -10, "vertical", "destroyer");
+      }).toThrow("ships must be placed entirely on the board");
+    });
+
+    test("place a cruiser at F4 horizontally", () => {
+      gameboard.placeShip("F", 4, "horizontal", "cruiser");
+      expect(gameboard.cruiser.coordinates).toEqual([
+        ["F", 4],
+        ["F", 5],
+        ["F", 6],
+      ]);
+    });
+
+    test("don't allow overlapping ships", () => {
+      gameboard.placeShip("F", 4, "horizontal", "cruiser");
+      expect(() => {
+        gameboard.placeShip("F", 3, "horizontal", "carrier");
+      }).toThrow("ships cannot overlap");
+    });
   });
 
-  test("gameboard class exists", () => {
-    expect(playerOneGameboard).toBeTruthy();
+  describe("receiveAttack tests", () => {
+    beforeEach(() => {
+      gameboard = new Gameboard();
+      gameboard.placeShip("A", 1, "horizontal", "carrier");
+      gameboard.placeShip("C", 3, "vertical", "cruiser");
+    });
+
+    test("tracks location of one hit", () => {
+      gameboard.receiveAttack("A", 1);
+      expect(gameboard.hits).toEqual([["A", 1]]);
+    });
+
+    test("tracks locations of multiple hits", () => {
+      gameboard.receiveAttack("A", 1);
+      gameboard.receiveAttack("D", 3);
+      expect(gameboard.hits).toEqual([
+        ["A", 1],
+        ["D", 3],
+      ]);
+    });
+
+    test("tracks location of misses", () => {
+      gameboard.receiveAttack("G", 1);
+      gameboard.receiveAttack("J", 3);
+      expect(gameboard.misses).toEqual([
+        ["G", 1],
+        ["J", 3],
+      ]);
+    });
+  });
+
+  describe("allShipsSunk tests", () => {
+    beforeEach(() => {
+      gameboard = new Gameboard();
+      gameboard.placeShip("A", 1, "horizontal", "carrier");
+      gameboard.placeShip("C", 3, "vertical", "cruiser");
+      gameboard.receiveAttack("A", 1);
+      gameboard.receiveAttack("A", 2);
+      gameboard.receiveAttack("A", 3);
+      gameboard.receiveAttack("A", 4);
+      gameboard.receiveAttack("A", 5);
+      gameboard.receiveAttack("C", 3);
+      gameboard.receiveAttack("D", 3);
+    });
+
+    test("reports false if not all ships are sunk", () => {
+      expect(gameboard.allShipsSunk()).toBeFalsy();
+    });
+
+    test("reports false if all ships are sunk", () => {
+      gameboard.receiveAttack("E", 3);
+      expect(gameboard.allShipsSunk()).toBeTruthy();
+    });
+  });
+});
+
+describe("player class tests", () => {
+  let player;
+
+  describe("player tests", () => {
+    beforeEach(() => {
+      player = new Player();
+    });
+
+    test("player exists", () => {
+      expect(player).toBeTruthy();
+    });
   });
 });
